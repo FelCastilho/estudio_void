0c37b4a6e922810900a6f4d1261b502d877ebbd4
felipeCast <castfelipe10@gmail.com>
2023-10-26 12:43:57 -0300

    Adicionando scrollreveal nos itens

---
+++ scripts/scrollreveal.js
@@ -0,0 +1,59 @@
+window.sr = ScrollReveal({reset: true});
+
+sr.reveal('#description-p', {
+    duration: 2000,
+    distance: '-10%'
+})
+
+sr.reveal('#astronaut-description', {
+    duration: 2000,
+    distance: '-10%'
+})
+
+sr.reveal('.first-comment', {
+    duration: 2000,
+    distance: '-20%'
+})
+
+sr.reveal('.second-comment',{
+    duration: 3000,
+    distance: '-20%'
+})
+
+sr.reveal('.third-comment',{
+    duration: 4000,
+    distance: '-20%'
+})
+
+sr.reveal('.void', {
+    duration: 2000,
+})
+
+sr.reveal('.constelacao', {
+    duration: 2500,
+})
+
+sr.reveal('.supernova', {
+    duration: 2700,
+})
+
+sr.reveal('#contact-img', {
+    duration: 2000,
+    distance: '-20%'
+})
+
+sr.reveal('#behance', {
+    duration: 2000,
+})
+
+sr.reveal('#linkedin', {
+    duration: 2500,
+})
+
+sr.reveal('#instagram', {
+    duration: 3000,
+})
+
+sr.reveal('#github', {
+    duration: 3500,
+})
